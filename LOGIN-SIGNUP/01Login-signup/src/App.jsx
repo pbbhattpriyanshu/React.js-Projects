b227474d0f6018a5b1095106faf77d6748909822
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-27 18:13:29 +0530

    Login and Sign Up project done

---
+++ LOGIN-SIGNUP/01Login-signup/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import LoginSign from './Components/LoginSign'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <LoginSign />
+    </>
+  )
+}
+
+export default App
